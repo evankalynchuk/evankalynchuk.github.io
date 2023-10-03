925a523eb20d32d2b54add8593e501d7cb633985
Evan Kalynchuk <Evan.Kalynchuk@ensignenergy.com>
2023-10-03 11:52:34 -0600

    class no wrap

--- egn-repeating-section-table.js
+++ egn-repeating-section-table.js
@@ -103,7 +103,7 @@ export class MyTable extends LitElement {
       </tr>
     `);
 
-    const headers = Object.keys(data[0]).map(header => (header == 'Item Description') ? html`<th>${header}</th>` : html`<th class="text-nowrap">${header}</th>`);
+    const headers = Object.keys(data[0]).map(header => (header == 'Item Description') ? html`<th class="text-nowrap">${header}</th>` : html`<th class="text-nowrap">${header}</th>`);
 
     const table = html`
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
